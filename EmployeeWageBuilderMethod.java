d3871d3826da163bd0dce3630d175e3ce28f7e85
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-06 20:30:03 +0530

    UC5 Added : 20 day wage

--- EmployeeWageBuilderMethod.java
+++ EmployeeWageBuilderMethod.java
@@ -1,32 +1,33 @@
 package com.employeepart2;
 
 public class EmployeeWageBuilderMethod
-{ 
-public static void main(String[] args){
-		final int IS_FULL_TIME = 1;
-		final int IS_PART_TIME = 2;
-		final int EMP_RATE_PER_HOUR = 20;
+{
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int IS_ABSENT = 0;
+	public static final int WAGE_PER_HOUR = 20;
+
+	public static void main(String[] args) {
+		int wrkHrs = 0;
+		int empCheck=(int)(Math.random()*10)%3;
+
+		switch(empCheck) {
+		case IS_FULL_TIME : wrkHrs=16;
+		System.out.println("Employee is Present Full Time and working hours is = "+wrkHrs);
+		break;
 		
-		int empHrs = 0;
-		double empCheck = Math.floor(Math.random() * 10 ) % 3;
-		
-		{
-		switch((int)empCheck) 
-		{
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		default:
-			empHrs = 0;
-			break;
+		case IS_PART_TIME : wrkHrs=8;
+		System.out.println("Employee is Part Time and working hours is = "+wrkHrs);
+		break;
+
+		case IS_ABSENT : wrkHrs=0;
+		System.out.println("Employee is Absent and working hours = "+wrkHrs);
+		break;
+
+		default:System.out.println("Invalid Input");
+		}
+		System.out.println("Employee's Daily Wage is = "+(wrkHrs*WAGE_PER_HOUR));
+
 	}
-}
-	{
-	int empSalary = empHrs * EMP_RATE_PER_HOUR;
-	System.out.println("Employee Wage = " +empSalary);
-	}
-	}
+
 }
