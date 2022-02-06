0390f97412773d7f2945b13a57da915921c9fa30
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-06 20:23:00 +0530

    UC3 Added : Wage as per Part Time Full Time

--- EmployeeWageBuilderMethod.java
+++ EmployeeWageBuilderMethod.java
@@ -1,26 +1,25 @@
 package com.employeepart2;
 
-import java.util.Random;
-
-public class EmployeeWageBuilderMethod 
-{
-	public static void main(String[] args) 
-{
+public class EmployeeWageBuilderMethod
+{ 
+public static void main(String[] args){
+	
+	int IS_FULL_TIME = 1;
+	int IS_PART_TIME = 2;
+	int EMP_RATE_PER_HOUR = 20;
 	int empHrs = 0;
-	int wagephr =20;
-	Random random = new Random();
-	int randomNum = random.nextInt(2);
+	int empWage = 0;
 	
-	if (randomNum == 1)
+	double empCheck = Math.floor(Math.random() * 10 ) % 3;
+	
 	{
-		System.out.println("Employee is Present");
-		empHrs = 8;
-	}
-	else
-		System.out.println("Employee is Absent");
 	
-	double salary = empHrs * wagephr ;
-	System.out.println("Employee Wage is : " +salary);
+		if (empCheck == IS_FULL_TIME )
+		empHrs = 8;
+		else if (empCheck == IS_PART_TIME )
+		empHrs = 4;
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	System.out.println("Employee Wage:" +empWage);
 	}
-
+  }
 }
